931638d39a18eda0fc68084527753f811e8d9ea5
Flex Ferrum <flexferrum@gmail.com>
2016-11-20 01:47:18 +0300

    Урок 0x03. Запрос символа операции

--- sources/lessons/lesson_0x03/simple_calc.cpp
+++ sources/lessons/lesson_0x03/simple_calc.cpp
@@ -5,9 +5,11 @@ int main()
 	double a = 0.0;
 	double b = 0.0;
 	
-	std::cin >> a >> b;
-
-	if (b != 0)
+	char ch = '\0';
+ 
+	std::cin >> a >> b >> ch;
+	
+ 	if (b != 0)
 		std::cout << a / b << std::endl;
 	else
 		std::cout << "Division by zero!" << std::endl;
